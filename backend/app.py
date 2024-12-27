4b30925ed05cf37838773f4104e8085dd8c59bd9
NenninaF <134356040+NenninaF@users.noreply.github.com>
2024-12-26 21:33:06 -0600

    fixed flask cors

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
+#To allow the react appp to fetch data from here
 @app.route("/")
 def home():
     return "Hi, Welcome to the International Job Website!"
